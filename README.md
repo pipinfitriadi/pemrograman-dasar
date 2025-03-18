85dd0441cacd6987a35866637645df2b119341fa
pipinfitriadi <pipinfitriadi@gmail.com>
2025-03-18 03:04:27 +0000

    docs: 5 Aturan Penulisan Sintaks Python yang Harus dipatuhi

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 * [Pengenalan Dasar Python dan Persiapan Awal](https://www.petanikode.com/python-linux/)
+* [5 Aturan Penulisan Sintaks Python yang Harus dipatuhi](https://www.petanikode.com/python-sintaks/)
+    * [aturan_sintaks.py](aturan_sintaks.py)
---
+++ aturan_sintaks.py
@@ -0,0 +1,26 @@
+judul = "Belajar Pemrograman Python sampai Bisa"
+penulis = 'Petani Kode'
+
+print(judul)
+print(penulis)
+
+print()
+
+pengenalan = """Nama: Pipin Fitriadi
+Tempat, Tanggal Lahir: ...
+Asal: ..."""
+
+print(pengenalan)
+
+print()
+
+print("hello")
+
+nama = 'petanikodes'
+
+if nama == 'petanikode':
+    print(nama)
+    print("selamat datang")
+
+# Contoh Komentar
+print("world") # Contoh Komentar
